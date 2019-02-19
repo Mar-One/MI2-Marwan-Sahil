f7718d2fbad370a94b44f22ad391bc1858bbec6d
marwansahil <32297119+Mar-One@users.noreply.github.com>
2019-02-19 18:38:45 +0100

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+let img = document.querySelector('#img-van');
+let button = document.querySelector('#sepia');
+button.addEventListener('click', function() {img.classList.add('sepia');
+});
+});
